6fbc8abbbe17c48848b08706fcf549e62d0af1c4
Louise Reid <l_reid@hotmail.co.uk>
2019-06-19 16:36:27 +0100

    removes hard coded initial spends

--- Budget Tracker/Models/SpendList.swift
+++ Budget Tracker/Models/SpendList.swift
@@ -13,32 +13,32 @@ class SpendList {
   var items: [SpendItem] = []
   
   init() {
-    let row0Spend = SpendItem()
-    let row1Spend = SpendItem()
-    let row2Spend = SpendItem()
-    let row3Spend = SpendItem()
-    let row4Spend = SpendItem()
-    
-    row0Spend.description = "Tesco Shop"
-    row0Spend.value = 30.50
-    
-    row1Spend.description = "Train Pass"
-    row1Spend.value = 16.50
-    
-    row2Spend.description = "McDonalds"
-    row2Spend.value = 5.60
-    
-    row3Spend.description = "Petrol"
-    row3Spend.value = 15.00
-    
-    row4Spend.description = "Gym"
-    row4Spend.value = 10.00
-    
-    items.append(row0Spend)
-    items.append(row1Spend)
-    items.append(row2Spend)
-    items.append(row3Spend)
-    items.append(row4Spend)
+//    let row0Spend = SpendItem()
+//    let row1Spend = SpendItem()
+//    let row2Spend = SpendItem()
+//    let row3Spend = SpendItem()
+//    let row4Spend = SpendItem()
+//    
+//    row0Spend.description = "Tesco Shop"
+//    row0Spend.value = 30.50
+//    
+//    row1Spend.description = "Train Pass"
+//    row1Spend.value = 16.50
+//    
+//    row2Spend.description = "McDonalds"
+//    row2Spend.value = 5.60
+//    
+//    row3Spend.description = "Petrol"
+//    row3Spend.value = 15.00
+//    
+//    row4Spend.description = "Gym"
+//    row4Spend.value = 10.00
+//    
+//    items.append(row0Spend)
+//    items.append(row1Spend)
+//    items.append(row2Spend)
+//    items.append(row3Spend)
+//    items.append(row4Spend)
   }
   
   func remove(_ item: SpendItem, at index: Int) {
